fccc28b1c200a31e81ac9789d7f7fc23b2fc6e8c
Martin <martin_d@mail.bg>
2022-06-01 11:28:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! UPDATE!!"
+    return "Welcome! UPDATE!! And one more UPDATE!!"
 
 @app.route('/how are you')
 def hello():
